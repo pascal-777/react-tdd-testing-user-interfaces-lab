fa0216e2f6a50f7d36e18e4108d01a1865278f69
pascal <pascal.nicintije@student.moringaschool.com>
2024-05-08 11:06:01 +0300

    "completes lab"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Kyra</h1>
+      <div>
+        <img
+          src="https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png"
+          alt="headshot"
+        />
+        <article>
+          <h2>About Me</h2>
+          <p>This is a paragraph about me.</p>
+          <a href="https://www.linkedin.com/in/kyra-hamerling-potts/">
+            Linkedin
+          </a>
+          <a href="https://github.com/khamerling-potts">Github</a>
+        </article>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,52 @@
-import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
-
+import { getAllByRole, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "../App";
 
 // Your tests here
+test("displays a top level heading with the test 'Hi, I'm _____'", () => {
+  render(<App />);
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+  });
+
+  expect(topLevelHeading).toBeInTheDocument();
+});
+
+test("displays headshot with 'headshot' as the alt text", () => {
+  render(<App />);
+  const headshotImg = screen.getByAltText(/headshot/i);
+  expect(headshotImg).toHaveAttribute(
+    "src",
+    "https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png"
+  );
+});
+
+test("has a second level heading with the text 'About Me'", () => {
+  render(<App />);
+  const aboutHeading = screen.getByRole("heading", {
+    name: /About Me/,
+    level: 2,
+  });
+  expect(aboutHeading).toBeInTheDocument();
+});
+test("displays paragraph for biography", () => {
+  render(<App />);
+  const bio = screen.getByText(/This is/i);
+  expect(bio).toBeInTheDocument();
+});
+test("displays two links to socials", () => {
+  render(<App />);
+  const link1 = screen.getByRole("link", {
+    name: /github/i,
+  });
+  const link2 = screen.getByRole("link", {
+    name: /linkedin/i,
+  });
+  expect(link1).toHaveAttribute("href", expect.stringContaining("github.com"));
+  expect(link2).toHaveAttribute(
+    "href",
+    expect.stringContaining("linkedin.com")
+  );
+});
